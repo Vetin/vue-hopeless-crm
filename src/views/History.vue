7d4aa56c5a27f91335004e9eef2fa90a2da8e4b4
Vetin <buffyc9h13n@gmail.com>
2020-05-12 21:24:22 +0300

    add chart to history page

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
 		</div>
 
 		<div class="history-chart">
-			<canvas></canvas>
+			<canvas ref="canvas"></canvas>
 		</div>
 		<Loader v-if="isLoading" />
 		<EmptyCategory v-else-if="records.length === 0" />
@@ -26,8 +26,9 @@
 </template>
 
 <script>
-import paginationMixin from '@/mixins/pagination.mixins';
+import { Pie } from 'vue-chartjs';
 import Table from '@/components/History/Table';
+import paginationMixin from '@/mixins/pagination.mixins';
 
 
 export default {
@@ -35,24 +36,79 @@ export default {
 		records: null,
 		isLoading: true,
 	}),
+	extends: Pie,
 	mixins: [paginationMixin],
 	components: {
 		Table
 	},
 	methods: {
+		async intiPage (categories, records)		{
+			this.setupPagination(this.records = Object.keys(records).map(key =>			{
+				const category = categories.filter(el => el.id === records[key].categoryId);
+				return {
+					...records[key],
+					id: key,
+					title: category[0].title
+				};
+			}));
+		},
+		arrayUnique (records)		{
+			for (let i = 0; i < records.length; i++) {
+				records.forEach((el, index) =>				{
+					if (i === index) {
+						return;
+					}
+					if (records[i].title === el.title) {
+						records.splice(index, 1);
+					}
+				});
+			}
+			return records;
+		},
+		initChart (categories, records)		{
+			const uniqRecords = this.arrayUnique(records);
+			this.renderChart({
+				labels: uniqRecords.map(record => { console.log(record); return record.title; }),
+				datasets: [{
+					label: 'Расходы по категориям',
+					data: uniqRecords.map(uniq =>					{
+						return records.reduce((acc, record) =>						{
+							if (record.categoryId === uniq.categoryId && record.type === 'outcome') {
+
+								acc += +record.price;
+							}
+							return acc;
+						}, 0);
+					}),
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
+				}]
+			}, {
+				responsive: true,
+				maintainAspectRatio: false
+			});
+		}
 	},
 	async mounted ()	{
-		const categories = await this.$store.dispatch('getCategories');
 		const records = await this.$store.dispatch('getRecords');
-		this.setupPagination(this.records = Object.keys(records).map(key =>		{
-			const category = categories.filter(el => el.id === records[key].categoryId);
-			return {
-				...records[key],
-				id: key,
-				title: category[0].title
-			};
-		}));
-
+		const categories = await this.$store.dispatch('getCategories');
+		await this.intiPage(categories, records);
+		this.initChart(categories, this.records);
 		this.isLoading = false;
 	}
 }
